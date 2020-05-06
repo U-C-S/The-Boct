3cf5e32c4797f3ad73bbcf817d6df92e778b3c23
U Chanakya S <50218121+devUCS@users.noreply.github.com>
2020-05-06 16:13:23 +0530

    v1.1

---
+++ botscript/bot.js
@@ -0,0 +1,17 @@
+
+//The death_botty Function
+function BOTTy() {
+  var name = prompt("Hoi, What's your name? \nDon't type nothing");
+   if(name != null) {var letters = name.length; }
+   var notnum = isNaN(name);
+
+    if(name === null) { alert('YOU LEGENDARY IDIOT!'); }
+    else if (name === 'nothing') { alert('You are basically NOTHING!!!'); }
+    else if (notnum === false) { alert('Type a name, YOU FOOL'); }
+    else if (letters < 4) { alert('I dont believe your name is "' + name + '"'); }
+    else if (notnum === true) { alert('So ' + name + ', You will have a Bright Future'); }
+
+
+
+ console.log("I'm DEATH_BOTTy, v1 created by @The-UCS-Variable")
+}
