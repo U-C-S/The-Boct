1ce3068a0110333f1ed8c3ce29ebe774d7cd2ca8
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-05-10 18:24:02 +0530

    v1.1.8.6

--- boctscript/boct.js
+++ boctscript/boct.js
@@ -5,14 +5,15 @@ function BOc() {
     document.getElementById("content-div-bot").style.margin="10px auto 20px";
     document.getElementById("chatter").style.display="block";
 
-    var name = prompt("Hoi, What's your name? \nDon't type nothing");
+    let name = prompt("Hoi, What's your name? \nDon't type nothing");
      if(name != null) {var letters = name.length; }
-     var notnum = isNaN(name);
+     let notnum = isNaN(name);
 
       if(name === null) { alert('YOU LEGENDARY IDIOT!'); }
       else if (name === 'nothing') { alert('You are basically NOTHING!!!'); }
       else if (notnum === false) { alert('Type a name, YOU FOOL'); }
       else if (letters < 4) { alert('I dont believe your name is "' + name + '"'); }
+      else if (name.indexOf(' name') !== -1 ) {alert('My name is BOcT'); }
       else if (notnum === true) { alert('So ' + name + ', You will have a Bright Future'); }
 
 
