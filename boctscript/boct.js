1dbaa98a71a4321fa3345b51191877436e37e7b6
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-05-15 17:20:32 +0530

    v1.1.9.5.1
    - BOcT Script Update v1.1.4

--- boctscript/boct.js
+++ boctscript/boct.js
@@ -1,19 +1,56 @@
 console.log("Font Awesome v5.13.0");
 console.log("Animations by animista.net")
+console.log("I'm 'The BOcT' v1.1.4, created by @The-UCS-Variable");
+/*------------------------------------------------------------------------------*/
 
 
+
+var click_count = 0;
 const CLICK_BOT = document.getElementById('BOcT');
 CLICK_BOT.addEventListener("click", BOcT_open);
 
 function BOcT_open() {
   document.getElementById("content-div-bot").style.margin="10px auto 20px";
   document.getElementById("chatter").style.display="block";
-  console.log("I'm 'The BOcT' v1.1.3, created by @The-UCS-Variable")
+  CLICK_BOT.removeEventListener("click", BOcT_open);
+  CLICK_BOT.addEventListener("click", BOcT_annoy_clicks);
+}
+
+var talkContent = 0;
+function talk_div_boct(talkContent) {
+  var newDIV_b = document.createElement('div');
+  var attr_b = document.createAttribute('class');
+  var talk_create = document.createElement('p');
+  var talk_matter = document.createTextNode(talkContent);
+
+  attr_b.value = "boct_talk recent_talk";
+  newDIV_b.setAttributeNode(attr_b);
+  document.getElementById('chatspace').appendChild(newDIV_b);
+  talk_create.appendChild(talk_matter);
+  var abcde = document.getElementsByClassName('boct_talk recent_talk');
+  abcde.appendChild(talk_matter);
+}
+
+function BOcT_annoy_clicks() {
+   click_count += 1;
+   if (click_count > 10) {
+     click_count = 1;
+     talk_div_boct('Stop annoying me');
+   }
 }
 
 
 
 
+/*
+--list of functions here--
+_____________________
+- BOcT_open
+- BOcT_annoy_clicks
+- talk_div_boct
+- talk_div_hooman
+
+*/
 
 
 /*
@@ -35,6 +72,11 @@ function BOcT_open() {
 
     document.getElementById('typespace-id').focus();
 
+
+
+  function random(number) {
+    return Math.floor(Math.random() * (number+1));
+  }
 
 //The Annoy me! function
 function annoyer() {  setInterval(function(){ alert("You are a IDIOT");}, 2000);  }
