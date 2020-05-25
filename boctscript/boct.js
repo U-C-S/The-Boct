d5a7b2885a4ef628062a774e365abfaaa833de29
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-05-25 11:19:56 +0530

    v1.1.9.9.1
    - Changed to loading screen animation to loading bar
    - Some CSS Animation timing tweaks
    - New file talk-script.js

--- boctscript/boct.js
+++ boctscript/boct.js
@@ -1,5 +1,5 @@
 console.log("I'm 'The BOcT' v1.2.3, created by @The-UCS-Variable");
-window.onload = setTimeout(stopLoading, 2137);
+window.onload = setTimeout(stopLoading, 2713);
 /*------------------------------------------------------------------------------*/
 function stopLoading() {
   document.getElementById("loader").style.display="none";
@@ -63,29 +63,6 @@ function talk_div_hooman() {
 //61 is proto
 
 
-function boct_study_chat(studycontent) {
-  if (studycontent === "annoy me") {
-    for (var i = 0; i < 20; i++) {
-      talk_div_boct("You're a IDIOT");
-    }
-  }
-  if(studycontent != null) {var letters = studycontent.length; }
-  let notnum = isNaN(studycontent);
-
-   if (notnum === false) { talk_div_boct('Type a name, YOU FOOL'); }
-   else if (studycontent === "age") {
-     var birthdate = new Date("05/06/2020");
-     var Difference_In_Time = Date.now() - birthdate.getTime();
-     var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-     talk_div_boct("Approx, " + Difference_In_Days + " days");
-   }
-   else if (studycontent === 'no') { talk_div_boct('You are basically NOTHING!!!'); }
-   else if (letters < 4) { talk_div_boct(`I dont believe your name is ${studycontent}`); }
-   else if (studycontent.indexOf('name') !== -1 ) { talk_div_boct('My name is BOcT'); }
-   else if (notnum === true) { talk_div_boct(`So ${studycontent}, You will have a Bright Future`); }
-}
-
-
 function BOcT_annoy_clicks() {
    click_count += 1;
    if (click_count === 10) {
@@ -120,52 +97,30 @@ function proto_boct_study_chat(studycontent) {
 
 
 
-/*
---list of functions here--
-_____________________
-- stopLoading
-- scroll_update
-- BOcT_open
-- talk_div_boct
-- talk_div_hooman
-- boct_study_chat
-- BOcT_annoy_clicks
 
 
-_____________________
-
---list of Variables here--
-_____________________
--
 
 
-/*
-console.log('working success');
----------------DUMP-------------------
-    document.getElementById('typespace-id').focus();
 
 
-  function random(number) {
-    return Math.floor(Math.random() * (number+1));
+function boct_study_chat(studycontent) {
+  if (studycontent === "annoy me") {
+    for (var i = 0; i < 20; i++) {
+      talk_div_boct("You're a IDIOT");
+    }
   }
+  if(studycontent != null) {var letters = studycontent.length; }
+  let notnum = isNaN(studycontent);
 
-The Annoy me! function
-click counter
-
-
-type Writer Effect
-_____________________
-var i = 0;
-var speed = 40;
-
-function typeWriter() {
-  if (i < talkContent.length) {
-    document.getElementById("demo").innerHTML += talkContent.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+   if (notnum === false) { talk_div_boct('Type a name, YOU FOOL'); }
+   else if (studycontent === "age") {
+     var birthdate = new Date("05/06/2020");
+     var Difference_In_Time = Date.now() - birthdate.getTime();
+     var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+     talk_div_boct("Approx, " + Difference_In_Days + " days");
+   }
+   else if (studycontent === 'no') { talk_div_boct('You are basically NOTHING!!!'); }
+   else if (letters < 4) { talk_div_boct(`I dont believe your name is ${studycontent}`); }
+   else if (studycontent.indexOf('name') !== -1 ) { talk_div_boct('My name is BOcT'); }
+   else if (notnum === true) { talk_div_boct(`So ${studycontent}, You will have a Bright Future`); }
 }
-_____________________
-unit convertors
-punchuation ignore
-*/
---
+++ boctscript/talk-script.js
@@ -0,0 +1,53 @@
+
+
+
+
+/*
+--list of functions here--
+_____________________
+- stopLoading
+- scroll_update
+- BOcT_open
+- talk_div_boct
+- talk_div_hooman
+- boct_study_chat
+- BOcT_annoy_clicks
+
+
+_____________________
+
+--list of Variables here--
+_____________________
+-
+
+
+/*
+console.log('working success');
+---------------DUMP-------------------
+    document.getElementById('typespace-id').focus();
+
+
+  function random(number) {
+    return Math.floor(Math.random() * (number+1));
+  }
+
+The Annoy me! function
+click counter
+
+
+type Writer Effect
+_____________________
+var i = 0;
+var speed = 40;
+
+function typeWriter() {
+  if (i < talkContent.length) {
+    document.getElementById("demo").innerHTML += talkContent.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+_____________________
+unit convertors
+punchuation ignore
+*/
