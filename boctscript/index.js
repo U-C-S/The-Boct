9a8f98b59cd238ebda7cb4660f3521249423750c
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-25 11:32:47 +0530

    v1.2.0.85 - Tweaks to CSS - Removed BOcT head animation

--- boctscript/index.js
+++ boctscript/index.js
@@ -8,8 +8,8 @@ function stopLoading() {
   document.getElementById("loader").style.display="none";
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-  document.getElementById('content-div-bot').style.animation = "flicker-in-1 2.3s linear forwards";
   document.getElementById('title').style.animation = "focus-in-expand 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+//document.getElementById('content-div-bot').style.animation = "flicker-in-1 2.3s linear forwards";
 }
 
 var click_count = 0;
@@ -30,3 +30,14 @@ function BOcT_annoy_clicks() {
     talk_div_boct("Stop annoying me!");
   }
 }
+
+
+
+/* CSS DUMP
+
+@keyframes flicker-in-1 {
+  0%, 10%, 10.2%, 20%, 20.6%, 30%, 30.6%, 45%, 55.1%, 57%, 60.1%, 65%, 75.1%, 77%, 85.1%, 86%, 86.2%, 99.1% {opacity: 0;}
+  10.1%, 20.1%, 30.1%, 30.5%, 45.1%, 50%, 55%, 57.1%, 60%, 65.1%, 75%, 77.1%, 85%, 86.1%, 86.3%, 99%, 99.2%, 100% {opacity: 1;}
+}
+
+*/
