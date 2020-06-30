043adea130c0f4451213848929e9e606c9f11e15
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-30 17:08:34 +0530

    v1.2.1.56.4

--- sw.js
+++ sw.js
@@ -2,8 +2,13 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('video-store').then(function(cache) {
       return cache.addAll([
-        '/The-BOct/',
-        'index.html',
+        '/The-BOcT/',
+        '/The-BOcT/index.html',
+        '/The-BOcT/boctscript/index.js',
+        '/The-BOcT/boctscript/talk-script.js',
+        '/The-BOcT/boctscript/boct.js',
+        '/The-BOcT/boctstyle/boct.css',
+        '/The-BOcT/boctstyle/boctmediaq.css',
       ]);
     })
   );
