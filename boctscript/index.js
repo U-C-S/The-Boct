f60da08f9ec554a8a8a292cab606c45788762a0f
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-07-02 13:50:30 +0530

    v1.2.1.63.1

--- boctscript/index.js
+++ boctscript/index.js
@@ -114,30 +114,3 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
-
-
-var networkDataReceived = false;
-
-startSpinner();
-
-// fetch fresh data
-var networkUpdate = fetch('/data.json').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  networkDataReceived = true;
-  updatePage(data);
-});
-
-// fetch cached data
-caches.match('/data.json').then(function(response) {
-  if (!response) throw Error("No data");
-  return response.json();
-}).then(function(data) {
-  // don't overwrite newer network data
-  if (!networkDataReceived) {
-    updatePage(data);
-  }
-}).catch(function() {
-  // we didn't get cached data, the network is our last hope:
-  return networkUpdate;
-}).catch(showErrorMessage).then(stopSpinner());
--- service-worker.js
+++ service-worker.js
@@ -17,11 +17,24 @@ self.addEventListener('install', function(e) {
 //https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#network_falling_back_to_the_cache
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('The-BOcT').then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
