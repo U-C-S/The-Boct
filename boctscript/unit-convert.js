6f5267eac6ce05843d18f568e617f98f607b08f9
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-07-30 11:09:42 +0530

    v2.225

--- boctscript/unit-convert.js
+++ boctscript/unit-convert.js
@@ -1,12 +1,11 @@
 function unit_convert(usertyped){
-    talk_div_boct('Unit Converter Feature: Work-In-Progress');
-
     const ucs_data = usertyped.split(' ');
     if(ucs_data[2] == 'cm' && ucs_data[4] == 'km'){
       const UCSed = ucs_data[1] / 100000;
       talk_div_boct(`${UCSed}km`);
     }
     else{
+      talk_div_boct('Unit Converter Feature: Work-In-Progress');
       talk_div_boct('Working conversion: Convert x cm in km');
     }
     //const UCS = require('convert-units');
