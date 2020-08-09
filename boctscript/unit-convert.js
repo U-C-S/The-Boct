5a08c8779e19b98718b6676faec249d600a4ece8
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-08-09 14:19:09 +0530

    v2.3 Alpha 3
    - Added the ability to converting other type of units like Area
    - More type of coversions can now be added easily

--- boctscript/unit-convert.js
+++ boctscript/unit-convert.js
@@ -4,62 +4,87 @@ function unit_convert(usertyped){
 }
 
 function TheConverter(x , a , b) {
-  let UL = Object.keys(Lengths);
+  const Types = [Lengths,Areas,Temperatures];
   var iput;
   var oput;
-  UL.forEach((e) =>{
-    if(a == Lengths[e].unit){
-      iput = Lengths[e].convert(x);
-    }
-    if(b == Lengths[e].unit){
-      oput = Lengths[e].convert(x);
-    }
-  });
+
+  for(i = 0; i < Types.length; i++) {
+    let type = Types[i];
+    let obj = Object.keys(type);
+
+    obj.forEach((j)=>{
+      if( type[j].unit.includes(a) ) { iput = type[j].con_factor; }
+      if( type[j].unit.includes(b) ) { oput = type[j].con_factor; }
+    });
+
+  }
+
   var final = (x * (iput / oput));
   return talk_div_boct(`${final} ${b}`);
 }
 
 
-
-
 const Lengths = {
   m: {
-    unit: 'm',
-    convert: (val)=>{ return val / 1; }
+    unit: ['m','meter','meters','metre'],
+    con_factor: 1
+  },
+  mm: {
+    unit: ['mm','millimeter','millimeters','millimetre'],
+    con_factor: 1/1000
   },
   cm: {
-    unit: 'cm',
-    convert: (val)=>{ return val / 100; }
+    unit: ['cm','centimeter','centimeters','centimetre'],
+    con_factor: 1/100
   },
   km: {
-    unit: 'km',
-    convert: (val)=>{ return val / 0.001; }
+    unit: ['km','kilometer','kilometers','kilometre'],
+    con_factor: 1000
+  },
+  inch: {
+    unit: ['in','inch','inches'],
+    con_factor: 1/39.37008
   },
   ft: {
-    unit: 'ft',
-    convert: (val)=>{ return val / 3.280839895; }
+    unit: ['ft','feet','foot'],
+    con_factor: 1/3.280839895
   },
   miles: {
-    unit: 'miles',
-    convert: (val)=>{ return val / 0.000621369 }
+    unit: ['miles','mile','mi'],
+    con_factor: 1/0.000621369
   },
   yards: {
-    unit: 'yard',
-    convert: (val)=>{ return val / 1.093613298 }
+    unit: ['yard','yd'],
+    con_factor: 1/1.093613298
   },
   lightyear: {
-    unit: 'lightyears',
-    convert: (val)=>{ return val / (1 / 9460730777119564)}
+    unit: ['lightyears','ly'],
+    con_factor: 9460730472580800
   }
 }
 
 const Areas = {
   sqm: {
     unit: ['sqm','m2'],
-    convert: (val)=>{ return val / 1; }
+    con_factor: 1
   },
   acre: {
     unit: ['acres','ac'],
-    convert: (val)=>{ return val / 0.00024710538; }
+    con_factor: 1 / 0.00024710538
+  }
+}
+
+const Temperatures = {
+  celsius: {
+    unit: ['C','celsius'],
+    con_factor: 1
+  },
+  fahrenheit: {
+    unit: ['F','fahrenheit'],
+    con_factor: 1
+  },
+  Kelvin: {
+    unit: ['K','kelvin'],
+    con_factor: 1
   }
 }
