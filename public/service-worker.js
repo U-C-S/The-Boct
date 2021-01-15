363b8787bc74ca627706e7a84d79f60aceec7721
U-C-S <ucs3721@outlook.com>
2021-01-15 11:16:40 +0530

    v3.4 - ServiceWorker Update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
-var cacheName = "boct-3.3.0";
-var files = [
+var CACHEname = "boct-3.4.0";
+var RUNTIME = "runtime";
+var FILES = [
   "/",
   "/index.html",
   "/styles/boct.css",
@@ -16,32 +17,47 @@ var files = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(files);
-    })
-  );
-});
-
-//Network falling back to the cache
-//Src-- https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#network_falling_back_to_the_cache
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
+    caches
+      .open(CACHEname)
+      .then((cache) => cache.addAll(FILES))
+      .then(self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (e) => {
+  const currentCaches = [CACHEname, RUNTIME];
   e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(
+      caches.match(e.request).then((cachedResponse) => {
+        if (cachedResponse) return cachedResponse;
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(e.request).then((response) => {
+            return cache.put(e.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
+
+//src: https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/basic
