6c716fcb9324516206437ee7c8369ff229a725ca
U-C-S <ucs3721@outlook.com>
2021-01-25 18:05:06 +0530

    Dark/Light Theme v4-alpha-5

--- src/components/aboutpanel.tsx
+++ src/components/aboutpanel.tsx
@@ -2,6 +2,55 @@ import React from "react";
 import { svg2, svg3, svg4 } from "../lib/svg-render";
 import "../styles/aboutpanel.css";
 
+class ThemeButton extends React.Component {
+  ChangeTheme() {
+    let htmlElem = document.documentElement;
+
+    if (htmlElem.dataset.theme === "dark") {
+      htmlElem.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    } else {
+      htmlElem.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.ChangeTheme} title="Toggle Dark/Light Mode">
+        <svg viewBox="0 0 512 512">
+          <path id="svg4" d={svg4} />
+        </svg>
+      </button>
+    );
+  }
+}
+
+class InfoButton extends React.Component {
+  render() {
+    return (
+      <button id="infoBtn" title="Info">
+        <svg viewBox="0 0 512 512">
+          <path id="svg3" d={svg3} />
+        </svg>
+      </button>
+    );
+  }
+}
+
+class SettingsButton extends React.Component {
+  render() {
+    return (
+      <button id="settingBtn" title="Settings">
+        <svg viewBox="0 0 512 512">
+          <path id="svg2" d={svg2} />
+        </svg>
+      </button>
+    );
+  }
+}
+
+
 class AboutPanel extends React.Component {
   render() {
     return (
@@ -9,21 +58,9 @@ class AboutPanel extends React.Component {
         <fieldset>
           <legend>About BOcT</legend>
           <div className="aboutbtns">
-            <button id="settingBtn" title="Settings">
-              <svg viewBox="0 0 512 512">
-                <path id="svg2" d={svg2} />
-              </svg>
-            </button>
-            <button id="infoBtn" title="Info">
-              <svg viewBox="0 0 512 512">
-                <path id="svg3" d={svg3} />
-              </svg>
-            </button>
-            <button id="ThemeBtn" data-themenow="dark" title="Toggle Dark/Light Mode">
-              <svg viewBox="0 0 512 512">
-                <path id="svg4" d={svg4} />
-              </svg>
-            </button>
+            <SettingsButton />
+            <InfoButton />
+            <ThemeButton />
           </div>
           <p id="releaseVer">Version 4.0 | Jan 2021</p>
           <button id="pwaInstallBtn">install</button>
